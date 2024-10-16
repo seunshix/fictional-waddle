6b58e75b5032620d0b1855d78bbc306a7b078f9b
adexs <adexs@SEUN>
2024-10-16 12:32:26 -0500

    type conversion continued

--- src/l27_type_conversions.cpp
+++ src/l27_type_conversions.cpp
@@ -55,6 +55,15 @@ using namespace std;
         (new_type) expression
         new_type (expression)
     ++++++++++++++++++- but each one with its own special characteristics:
+
+    dynamic_cast
+    ============
+    dynamic_cast can only be used with pointers and references to classes or with void*. Its purpose is to ensure that the result 
+    of the type conversion points to a valid complete object of the destination pointer type.
+
+    This naturally includes pointer upcast(converting from pointer-derived to pointer-to-base) in the same way as allowed in an
+    implicit conversion. But dynamic_cast can also downcast(convert from pointer-to-base to pointer-to-derived) polymorphic classes
+    (those with virtual members) if-and only if the pointed object is a valid complete object of the target type.
 */
 
 class A{};
